09f033966c3ad84e2512feccb3b940d5faa5f94d
weisj <weisj@meixing.com>
2020-12-04 15:21:44 +0800

    [remember是多余的]

--- app/src/main/java/com/example/testcompose/NavGraph.kt
+++ app/src/main/java/com/example/testcompose/NavGraph.kt
@@ -2,7 +2,6 @@ package com.example.testcompose
 
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.Immutable
-import androidx.compose.runtime.remember
 
 /** 页面路径 */
 sealed class Destination {
@@ -16,10 +15,10 @@ sealed class Destination {
     /** 路径到页面的映射 */
     @Composable
     fun Page() {
-        when (val d = remember { this }) {
+        when (this) {
             Launcher -> LaunchPage()
             Home -> HomePage()
-            is Second -> SecondPage(d.bottom)
+            is Second -> SecondPage(this.bottom)
         }
     }
 }
